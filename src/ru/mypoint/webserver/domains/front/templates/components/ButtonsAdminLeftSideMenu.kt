40625975c6a4f9c9076d2bae5b53ac7180d9355d
Pavel Aksenov <sagit117@gmail.com>
2021-12-20 08:51:18 +0300

    пункты меню

--- src/ru/mypoint/webserver/domains/front/templates/components/ButtonsAdminLeftSideMenu.kt
+++ src/ru/mypoint/webserver/domains/front/templates/components/ButtonsAdminLeftSideMenu.kt
@@ -6,6 +6,30 @@ import kotlinx.html.*
 class ButtonsAdminLeftSideMenu: Template<FlowContent> {
 
     override fun FlowContent.apply() {
+        div {
+            classes = setOf("side_menu__group")
 
+            h3 {
+                classes = setOf("side_menu__group__header")
+                +"Администрирование"
+            }
+
+            div {
+                classes = setOf("side_menu__group__items")
+
+                div {
+                    classes = setOf("side_menu__group__item")
+
+                    img {
+                        src = "/static/users.svg"
+                        classes = setOf("mr-1")
+                    }
+                    a {
+                        href = "/admin/panel/users"
+                        +"Пользователи"
+                    }
+                }
+            }
+        }
     }
 }
